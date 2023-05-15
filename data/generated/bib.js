e3a92e35e5836873f803cf64f35d8587e72db75c
Aryavrat4 <133676033+Aryavrat4@users.noreply.github.com>
2023-05-15 15:49:54 +0100

    Add files via upload

---
+++ data/generated/bib.js
@@ -0,0 +1,112 @@
+﻿define({ entries : {
+    "7313103": {
+        "author": "Van Oort, Niels and Cats, Oded",
+        "booktitle": "2015 IEEE 18th International Conference on Intelligent Transportation Systems",
+        "doi": "10.1109/ITSC.2015.11",
+        "number": "",
+        "pages": "19-24",
+        "title": "Improving Public Transport Decision Making, Planning and Operations by Using Big Data: Cases from Sweden and the Netherlands",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2015"
+    },
+    "8386527": {
+        "author": "Zhang, Sijia and Jia, Shunping and Ma, Cunrui and Wang, Yuqiong",
+        "booktitle": "2018 IEEE 3rd International Conference on Cloud Computing and Big Data Analysis (ICCCBDA)",
+        "doi": "10.1109/ICCCBDA.2018.8386527",
+        "number": "",
+        "pages": "280-284",
+        "title": "Impacts of public transportation fare reduction policy on urban public transport sharing rate based on big data analysis",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2018"
+    },
+    "8883692": {
+        "author": "Zhu, Huaizhong and Yang, Xiaoguang and Wang, Yizhe and Zhang, Nan",
+        "booktitle": "2019 5th International Conference on Transportation Information and Safety (ICTIS)",
+        "doi": "10.1109/ICTIS.2019.8883692",
+        "number": "",
+        "pages": "85-91",
+        "title": "The Prediction of Multimodal Public Transportation Sharing Rate Based on Data",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2019"
+    },
+    "9343362": {
+        "author": "Alicea, Michael and McDonald, Arlen P. and Tang, Chunyang and Yang, Jeong",
+        "booktitle": "2020 IEEE 14th International Conference on Big Data Science and Engineering (BigDataSE)",
+        "doi": "10.1109/BigDataSE50710.2020.00011",
+        "number": "",
+        "pages": "22-27",
+        "title": "Exploring the Application of Machine Learning Algorithms to the City Public Bus Transport",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2020"
+    },
+    "9425771": {
+        "author": "Ye, Li and Thiengburanathum, Pree and Thiengburanathum, Poon",
+        "booktitle": "2021 Joint International Conference on Digital Arts, Media and Technology with ECTI Northern Section Conference on Electrical, Electronics, Computer and Telecommunication Engineering",
+        "doi": "10.1109/ECTIDAMTNCON51128.2021.9425771",
+        "number": "",
+        "pages": "243-248",
+        "title": "A Real-Time Bus Arrival Time Prediction System Based on Spark Framework and Machine Learning Approaches: a case study in Chiang Mai",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2021"
+    },
+    "9454833": {
+        "author": "Khunsri, Kavepol and Panichpapiboon, Sooksan",
+        "booktitle": "2021 18th International Conference on Electrical Engineering/Electronics, Computer, Telecommunications and Information Technology (ECTI-CON)",
+        "doi": "10.1109/ECTI-CON51831.2021.9454833",
+        "number": "",
+        "pages": "39-42",
+        "title": "A Big Data Analysis on Efficiency of Bangkok Taxi System",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2021"
+    },
+    "9498622": {
+        "author": "do Vale Saraiva, Tiago and Campos, Carlos Alberto Vieira",
+        "booktitle": "2021 International Wireless Communications and Mobile Computing (IWCMC)",
+        "doi": "10.1109/IWCMC51323.2021.9498622",
+        "number": "",
+        "pages": "675-680",
+        "title": "An approach using machine learning and public data to detect traffic jams",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2021"
+    },
+    "9529328": {
+        "author": "Taparia, Ankit and Brady, Mike",
+        "booktitle": "2021 7th International Conference on Models and Technologies for Intelligent Transportation Systems (MT-ITS)",
+        "doi": "10.1109/MT-ITS49943.2021.9529328",
+        "number": "",
+        "pages": "1-6",
+        "title": "Bus Journey and Arrival Time Prediction based on Archived AVL/GPS data using Machine Learning",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2021"
+    },
+    "9674960": {
+        "author": "Asad, Syed Muhammad and Dashtipour, Kia and Rais, Rao Naveed Bin and Hussain, Sajjad and Abbasi, Qammer Hussain and Imran, Muhammad Ali",
+        "booktitle": "2021 International Conference on UK-China Emerging Technologies (UCET)",
+        "doi": "10.1109/UCET54125.2021.9674960",
+        "number": "",
+        "pages": "235-240",
+        "title": "Employing Machine Learning for Predicting Transportation Modes Under the COVID-19 Pandemic: A Mobility-Trends Analysis",
+        "type": "INPROCEEDINGS",
+        "volume": "",
+        "year": "2021"
+    },
+    "9846898": {
+        "author": "Lapamonpinyo, Pipatphon and Derrible, Sybil and Corman, Francesco",
+        "doi": "10.1109/OJITS.2022.3194879",
+        "journal": "IEEE Open Journal of Intelligent Transportation Systems",
+        "number": "",
+        "pages": "539-550",
+        "title": "Real-Time Passenger Train Delay Prediction Using Machine Learning: A Case Study With Amtrak Passenger Train Routes",
+        "type": "ARTICLE",
+        "volume": "3",
+        "year": "2022"
+    }
+}});
